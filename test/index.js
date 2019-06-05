93cb415254dd4b8edb3ded91cdeb8c49205c9b5f
Étienne Brouillard <etienne.brouillard@outlook.com>
2019-06-05 10:53:27 -0400

    fix unittest reporting

--- test/index.js
+++ test/index.js
@@ -16,8 +16,10 @@ const testRunner = require('vscode/lib/testrunner');
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
 // for more info
 testRunner.configure({
-    "ui": 'tdd', 		
-    "useColors": true 
+    "ui": 'tdd',
+    "useColors": true,
+    "reporter": "mocha-junit-reporter",
+    "reporterOptions": {"mochaFile": "./test/test-results.xml"}
 });
 
 module.exports = testRunner;
